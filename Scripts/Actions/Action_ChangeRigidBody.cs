4c76d01013b0a35be259751908615fbe4b52528a
Diogo Andrade <diogo.andrade@mind.pt>
2023-01-14 19:05:28 +0000

    Improved kit, added new sample game (Breakout)
    Added new actions: Change Rigid Body properties, Set Animation Parameter, Set Parent
    Improved actions: Destroy Object, Random, Set Velocity and Spawn
    Improved condition trigger

--- Scripts/Actions/Action_ChangeRigidBody.cs
+++ Scripts/Actions/Action_ChangeRigidBody.cs
@@ -5,14 +5,17 @@ using NaughtyAttributes;
 
 public class Action_ChangeRigidBody : Action
 {
-    [SerializeField] private enum ChangeType { SetBodyType };
+    [SerializeField] private enum ChangeType { SetBodyType, Mass, LinearDrag, AngularDrag, GravityScale };
 
     [SerializeField] 
     private ChangeType         changeType;
     [SerializeField, ShowIf("needsBodyType")] 
     private RigidbodyType2D    bodyType;
+    [SerializeField, ShowIf("needValue")]
+    private float              value;
 
     private bool needsBodyType => (changeType == ChangeType.SetBodyType);
+    private bool needValue => (changeType == ChangeType.Mass) || (changeType == ChangeType.LinearDrag) || (changeType == ChangeType.AngularDrag) || (changeType == ChangeType.GravityScale);
 
     public override void Execute()
     {
@@ -26,6 +29,18 @@ public class Action_ChangeRigidBody : Action
             case ChangeType.SetBodyType:
                 rb.bodyType = bodyType;
                 break;
+            case ChangeType.Mass:
+                rb.mass= value;
+                break;
+            case ChangeType.LinearDrag:
+                rb.drag = value;
+                break;
+            case ChangeType.AngularDrag:
+                rb.angularDrag = value;
+                break;
+            case ChangeType.GravityScale:
+                rb.gravityScale = value;
+                break;
             default:
                 break;
         }
@@ -37,6 +52,14 @@ public class Action_ChangeRigidBody : Action
         {
             case ChangeType.SetBodyType:
                 return $"Changes body type of this object to {bodyType}";
+            case ChangeType.Mass:
+                return $"Changes mass of this object to {value}";
+            case ChangeType.LinearDrag:
+                return $"Changes linear drag of this object to {value}";
+            case ChangeType.AngularDrag:
+                return $"Changes angular drag of this object to {value}";
+            case ChangeType.GravityScale:
+                return $"Changes gravity scale of this object to {value}";
         }
 
         return "Changes something on rigid body (change type not defined)!";
