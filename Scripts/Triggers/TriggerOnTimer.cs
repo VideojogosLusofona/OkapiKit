9879347db9988c2f898a9cf3a3e88c7af5942e13
Diogo Andrade <diogo.andrade@mind.pt>
2023-01-19 10:02:22 +0000

    Started the Side Shooter game sample

--- Scripts/Triggers/TriggerOnTimer.cs
+++ Scripts/Triggers/TriggerOnTimer.cs
@@ -5,18 +5,29 @@ using NaughtyAttributes;
 
 public class TriggerOnTimer : Trigger
 {
+    [SerializeField]
+    private bool    startTriggered = false;
     [SerializeField, MinMaxSlider(0.0f, 10.0f)]
-    private Vector2         timeInterval = new Vector2(5.0f, 5.0f);
+    private Vector2 timeInterval = new Vector2(5.0f, 5.0f);
 
     private float timer;
 
     protected override string GetRawDescription()
     {
-        return $"Every [{timeInterval.x},{timeInterval.y}] seconds, trigger the actions.";
+        if (timeInterval.x == timeInterval.y)
+        {
+            return $"Every {timeInterval.x} seconds, trigger the actions:";
+        }
+
+        return $"Every [{timeInterval.x},{timeInterval.y}] seconds, trigger the actions:";
     }
 
     void Start()
     {
+        if (startTriggered)
+        {
+            ExecuteTrigger();
+        }
         timer = Random.Range(timeInterval.x, timeInterval.y);        
     }
 
