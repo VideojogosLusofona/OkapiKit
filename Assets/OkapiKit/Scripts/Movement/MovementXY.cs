f026d6235ceeedde701c3006aee786989b0da565
Diogo de Andrade <diogo.g.andrade@gmail.com>
2024-11-02 15:49:05 +0000

    Added animator control on Movement XY

--- Assets/OkapiKit/Scripts/Movement/MovementXY.cs
+++ Assets/OkapiKit/Scripts/Movement/MovementXY.cs
@@ -8,6 +8,12 @@ namespace OkapiKit
     {
         public enum InputType { Axis = 0, Button = 1, Key = 2 };
         public enum Axis { UpAxis = 0, RightAxis = 1 };
+        public enum FlipBehaviour
+        {
+            None = 0,
+            VelocityFlipsSprite = 1, VelocityInvertsScale = 2,
+            VelocityRotatesSprite = 3
+        };
 
         [SerializeField]
         private Vector2 speed = new Vector2(100, 100);
@@ -51,9 +57,24 @@ namespace OkapiKit
         private bool inertiaEnable = false;
         [SerializeField]
         private float inertiaStopTime = 0.0f;
+        [SerializeField]
+        private FlipBehaviour flipBehaviour = FlipBehaviour.None;
+        [SerializeField]
+        private bool useAnimator = false;
+        [SerializeField]
+        private Animator animator;
+        [SerializeField, AnimatorParam("animator", AnimatorControllerParameterType.Float)]
+        private string horizontalVelocityParameter;
+        [SerializeField, AnimatorParam("animator", AnimatorControllerParameterType.Float)]
+        private string absoluteHorizontalVelocityParameter;
+        [SerializeField, AnimatorParam("animator", AnimatorControllerParameterType.Float)]
+        private string verticalVelocityParameter;
+        [SerializeField, AnimatorParam("animator", AnimatorControllerParameterType.Float)]
+        private string absoluteVerticalVelocityParameter;
 
         Vector3 moveVector;
         Vector3 currentVelocity = Vector3.zero;
+        SpriteRenderer spriteRenderer;
 
         public override Vector2 GetSpeed() => speed;
         public override void SetSpeed(Vector2 speed) { this.speed = speed; }
@@ -148,6 +169,45 @@ namespace OkapiKit
                     }
                 }
             }
+
+            string animDesc = "";
+
+            switch (flipBehaviour)
+            {
+                case FlipBehaviour.None:
+                    break;
+                case FlipBehaviour.VelocityFlipsSprite:
+                    animDesc += "When the character is moving to the left, the sprite renderer will be flipped.\n";
+                    break;
+                case FlipBehaviour.VelocityInvertsScale:
+                    animDesc += "When the character is moving to the left, the horizontal scale of this object will be inverted.\n";
+                    break;
+                case FlipBehaviour.VelocityRotatesSprite:
+                    animDesc += "When the character is moving to the left, the object will be rotated 180 degrees around the Y axis.\n";
+                    break;
+                default:
+                    break;
+            }
+
+            if (useAnimator)
+            {
+                Animator anim = animator;
+                if (anim == null) anim = GetComponent<Animator>();
+                if (anim)
+                {
+                    animDesc += $"Some values will be set on animator {anim.name}:\n";
+                    if (horizontalVelocityParameter != "") animDesc += $"Horizontal velocity will be set to parameter {horizontalVelocityParameter}.\n";
+                    if (absoluteHorizontalVelocityParameter != "") animDesc += $"Absolute horizontal velocity will be set to parameter {absoluteHorizontalVelocityParameter}.\n";
+                    if (verticalVelocityParameter != "") animDesc += $"Vertical velocity will be set to parameter {verticalVelocityParameter}.\n";
+                    if (absoluteVerticalVelocityParameter != "") animDesc += $"Absolute vertical velocity will be set to parameter {absoluteVerticalVelocityParameter}.\n";
+                }
+            }
+
+            if (animDesc != "")
+            {
+                desc += animDesc;
+            }
+
             return desc;
         }
 
@@ -165,6 +225,53 @@ namespace OkapiKit
                     CheckButton("Negative vertical button", verticalButtonPositive);
                 }
             }
+
+            if (flipBehaviour == FlipBehaviour.VelocityFlipsSprite)
+            {
+                spriteRenderer = GetComponentInParent<SpriteRenderer>();
+                if (spriteRenderer == null)
+                {
+                    _logs.Add(new LogEntry(LogEntry.Type.Error, "Flip behaviour is set to flip the sprite, but sprite doesn't exist!", "Flip behaviour acts on sprite, but no sprite is present!"));
+                }
+            }
+
+            if (useAnimator)
+            {
+                Animator anm = animator;
+                if (anm == null)
+                {
+                    anm = GetComponent<Animator>();
+                    if (anm == null)
+                    {
+                        _logs.Add(new LogEntry(LogEntry.Type.Error, "Animator not defined!", "If we want to drive an animator with the properties of the movement, we need to define which animator to use."));
+                    }
+                    else
+                    {
+                        _logs.Add(new LogEntry(LogEntry.Type.Warning, "Animator exists, but it should be linked explicitly!", "Setting options explicitly is always better than letting the system find them, since it might have to guess our intentions."));
+                    }
+                }
+                if (anm != null)
+                {
+                    if (anm.runtimeAnimatorController == null)
+                    {
+                        _logs.Add(new LogEntry(LogEntry.Type.Error, "Animator controller is not set!", "There's an animator, but it doesn't have a controller setup. Creat one and set it on the animator."));
+                    }
+                    else
+                    {
+                        Helpers.CheckErrorAnim(anm, "horizontal velocity", horizontalVelocityParameter, AnimatorControllerParameterType.Float, _logs);
+                        Helpers.CheckErrorAnim(anm, "absolute horizontal velocity", absoluteHorizontalVelocityParameter, AnimatorControllerParameterType.Float, _logs);
+                        Helpers.CheckErrorAnim(anm, "vertical velocity", verticalVelocityParameter, AnimatorControllerParameterType.Float, _logs);
+                        Helpers.CheckErrorAnim(anm, "absolute vertical velocity", absoluteVerticalVelocityParameter, AnimatorControllerParameterType.Float, _logs);
+                    }
+                }
+            }
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         void FixedUpdate()
@@ -260,6 +367,38 @@ namespace OkapiKit
             else
             {
                 moveVector = speed;
+            }
+
+            const float epsilonZero = 1e-3f;
+            switch (flipBehaviour)
+            {
+                case FlipBehaviour.None:
+                    break;
+                case FlipBehaviour.VelocityFlipsSprite:
+                    if (spriteRenderer)
+                    {
+                        if (currentVelocity.x > epsilonZero) spriteRenderer.flipX = false;
+                        else if (currentVelocity.x < -epsilonZero) spriteRenderer.flipX = true;
+                    }
+                    break;
+                case FlipBehaviour.VelocityInvertsScale:
+                    if ((currentVelocity.x > epsilonZero) && (transform.localScale.x < 0.0f)) transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+                    else if ((currentVelocity.x < -epsilonZero) && (transform.localScale.x > 0.0f)) transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+                    break;
+                case FlipBehaviour.VelocityRotatesSprite:
+                    if ((currentVelocity.x > epsilonZero) && (transform.right.x < 0.0f)) transform.rotation *= Quaternion.Euler(0, 180, 0);
+                    else if ((currentVelocity.x < -epsilonZero) && (transform.right.x > 0.0f)) transform.rotation *= transform.rotation *= Quaternion.Euler(0, 180, 0);
+                    break;
+                default:
+                    break;
+            }
+
+            if ((useAnimator) && (animator))
+            {
+                if (horizontalVelocityParameter != "") animator.SetFloat(horizontalVelocityParameter, currentVelocity.x);
+                if (absoluteHorizontalVelocityParameter != "") animator.SetFloat(absoluteHorizontalVelocityParameter, Mathf.Abs(currentVelocity.x));
+                if (verticalVelocityParameter != "") animator.SetFloat(verticalVelocityParameter, currentVelocity.y);
+                if (absoluteVerticalVelocityParameter != "") animator.SetFloat(absoluteVerticalVelocityParameter, Mathf.Abs(currentVelocity.y));
             }
         }
     }
